75f8b700fd9750d4c26203903650e4f9ae40fff2
celebwikitrends9-sketch <celebwikitrends9@gmail.com>
2025-08-25 16:28:20 +0530

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -21,14 +21,7 @@ export default function Header() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center justify-between">
         <div className="flex items-center gap-2">
-          <Link href="/" className="flex items-center gap-2">
-            {/* <Frame className="h-6 w-6 text-blue-600" /> */}
-             
-         
-          />
-        
-
-          </Link>
+          
         </div>
         <nav className="hidden md:flex items-center gap-6">
           <Link href="/" className="text-sm font-medium transition-colors hover:text-blue-600">
